b0111713e46412d9cde9ae665dbb1c8aba6b3406
smudgegame <atmargraff@gmail.com>
2022-06-02 21:38:34 -0400

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+import com.soywiz.korge.gradle.*
+
+buildscript {
+	val korgePluginVersion: String by project
+
+	repositories {
+		mavenLocal()
+		mavenCentral()
+		google()
+		maven { url = uri("https://plugins.gradle.org/m2/") }
+	}
+	dependencies {
+		classpath("com.soywiz.korlibs.korge.plugins:korge-gradle-plugin:$korgePluginVersion")
+	}
+}
+
+apply<KorgeGradlePlugin>()
+
+korge {
+	id = "org.maak.satelite"
+// To enable all targets at once
+
+	//targetAll()
+
+// To enable targets based on properties/environment variables
+	//targetDefault()
+
+// To selectively enable targets
+
+	targetJvm()
+	targetJs()
+}
